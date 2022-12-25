626552fdfa2209df539f365796a2086282d66249
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-26 03:17:27 +0900

    chap3 완료

--- test/src/main/java/com/sparta/test/Dto/ChatRoom.java
+++ test/src/main/java/com/sparta/test/Dto/ChatRoom.java
@@ -4,11 +4,17 @@ package com.sparta.test.Dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
 
--- test/src/main/java/com/sparta/test/controller/ChatController.java
+++ test/src/main/java/com/sparta/test/controller/ChatController.java
@@ -1,7 +1,10 @@
 package com.sparta.test.controller;
 
 import com.sparta.test.Dto.ChatMessage;
+import com.sparta.test.pubsub.RedisPublisher;
+import com.sparta.test.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -10,12 +13,22 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    @Autowired
+    private final RedisPublisher redisPublisher;
 
+    @Autowired
+    private final ChatRoomRepository chatRoomRepository;
+
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
