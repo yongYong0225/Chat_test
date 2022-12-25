1ce0a8f71d0fa287f445bc6a4b93d0f26c0533de
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-25 20:57:04 +0900

    feat:test

---
+++ test/src/main/java/com/sparta/test/Dto/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.sparta.test.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    //메세지 타입 : 입장, 채팅
+    public enum MessageType {
+        ENTER, TALK, JOIN
+    }
+
+    private MessageType type; //메세지 타입
+    private String roomId; //방번호
+    private String sender; //보낸사람
+    private String message; //메세지
+}
---
+++ test/src/main/java/com/sparta/test/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.sparta.test.controller;
+
+import com.sparta.test.Dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
+}
